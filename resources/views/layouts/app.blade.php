ca1c4306cb8987226870189ed124a6f4028272d7
Nikunj Kabariya <nikunjkumar.kabariya@brainvire.com>
2017-01-18 14:36:19 +0530

    In blade, css js path changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="../public/css/app.css" rel="stylesheet">
+    <link href="{{ asset("css/app.css") }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
@@ -77,11 +77,10 @@
                 </div>
             </div>
         </nav>
-
         @yield('content')
     </div>
 
     <!-- Scripts -->
-    <script src="../public/js/app.js"></script>
+    <script src="{{ asset("js/app.js") }}"></script>
 </body>
 </html>
